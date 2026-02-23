f8f893a896f13390ff6d2e2295e4b031495dd05c
Thameera08 <thamilahiru@gmail.com>
2026-02-23 17:20:48 +0530

    update

--- src/services/api/client.js
+++ src/services/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { clearToken, getToken } from '../../utils/auth'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5050/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
